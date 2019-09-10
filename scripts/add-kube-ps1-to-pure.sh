d75f7ab669e1f691c2ce16190508d5398aff053c
Jan Kuehle <jan.kuehle@red-badger.com>
2019-09-10 09:59:12 +0100

    Fix on Mac

--- scripts/add-kube-ps1-to-pure.sh
+++ scripts/add-kube-ps1-to-pure.sh
@@ -3,26 +3,26 @@
 set -e
 
 realpath() {
-    perl -MCwd -le 'print Cwd::abs_path(shift)' "$1"
+	perl -MCwd -le 'print Cwd::abs_path(shift)' "$1"
 }
 
 scriptpath=$(dirname "$0")
 rootpath=$(realpath "$scriptpath/..")
 
-sed "/^\tpreprompt_parts+=('%F{\${prompt_pure_colors\\[path\\]}}%~%f')\$/a\\
+sed "/^	preprompt_parts+=('%F{\${prompt_pure_colors\\[path\\]}}%~%f')\$/a\\
 \\
-\t# kube-ps1\\
-\tpreprompt_parts+=('\$(kube_ps1)')\\
+	# kube-ps1\\
+	preprompt_parts+=('\$(kube_ps1)')\\
 
 /^prompt_pure_setup() {\$/a\\
-\t# kube-ps1\\
-\tsource '$rootpath/modules/kube-ps1/kube-ps1.sh'\\
-\tKUBE_PS1_SEPARATOR='%F{cyan}/%f'\\
-\tKUBE_PS1_DIVIDER='%F{cyan}/%f'\\
-\tKUBE_PS1_SYMBOL_COLOR=cyan\\
-\tKUBE_PS1_CTX_COLOR=cyan\\
-\tKUBE_PS1_NS_COLOR=cyan\\
-\tKUBE_PS1_PREFIX=''\\
-\tKUBE_PS1_SUFFIX=''\\
+	# kube-ps1\\
+	source '$rootpath/modules/kube-ps1/kube-ps1.sh'\\
+	KUBE_PS1_SEPARATOR='%F{cyan}/%f'\\
+	KUBE_PS1_DIVIDER='%F{cyan}/%f'\\
+	KUBE_PS1_SYMBOL_COLOR=cyan\\
+	KUBE_PS1_CTX_COLOR=cyan\\
+	KUBE_PS1_NS_COLOR=cyan\\
+	KUBE_PS1_PREFIX=''\\
+	KUBE_PS1_SUFFIX=''\\
 \\
 " "$rootpath/modules/pure/pure.zsh"
